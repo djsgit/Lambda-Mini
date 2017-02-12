09b43c93d41f9e20bd2c4858edb7c2b4ff9b7a98
djsgit <djsgit@users.noreply.github.com>
2017-02-11 16:25:59 -0800

    Add files via upload
    . venv/bin/activate
    export FLASK_APP=yourfilename.py
    flask run

---
+++ my_blog.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+# @ signifies a decorator - way to wrap a function and modify it's behavior
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/birthday')
+def birthday():
+    return 'February 21 1985'
+
+@app.route('/greeting/<name>')
+def greeting(name):
+    return 'Hello, %s' % name
+
+@app.route('/sum/<int1>/<int2>')
+def sum(int1, int2):
+    return str(int(int1) + int(int2))
+
+@app.route('/multiply/<int1>/<int2>')
+def prod(int1, int2):
+    return str(int(int1) * int(int2))
+
+@app.route('/subtract/<int1>/<int2>')
+def diff(int1, int2):
+    total = int(int1) - int(int2)
+    return str(int(int1) - int(int2))
+
+@app.route('/favoritefoods')
+def myList():
+    myList = ['Sashimi', 'Pizza', 'Watermelon']
+    return jsonify(myList)
